e73e90a42222237c5114f4f37822466cc9089e10
Henrique Custódia <martinshenrique8@gmail.com>
2026-05-30 17:07:43 -0300

    feat: adiciona caso de uso usando MetadataReducer.list()

--- src/app/features/8.metadata/metadata.component.ts
+++ src/app/features/8.metadata/metadata.component.ts
@@ -1,10 +1,22 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import { createMetadataKey, form, FormField, maxLength, metadata, minLength, required } from '@angular/forms/signals';
+import {
+  createMetadataKey,
+  form,
+  FormField,
+  maxLength,
+  metadata,
+  MetadataReducer,
+  minLength,
+  required,
+} from '@angular/forms/signals';
 
 const USERNAME_HELP = createMetadataKey<string>();
 
+const HELP_LIST = createMetadataKey<string, string[]>(MetadataReducer.list());
+
 interface FormModel {
   username: string;
+  password: string;
 }
 
 @Component({
@@ -15,15 +27,15 @@ interface FormModel {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Metadata {
-
   protected readonly USERNAME_HELP = USERNAME_HELP;
+  protected readonly HELP_LIST = HELP_LIST;
 
   protected formModel = signal<FormModel>({
     username: '',
+    password: '',
   });
 
   protected form = form(this.formModel, (schema) => {
-
     required(schema.username, { message: 'Username é obrigatório' });
     minLength(schema.username, 5, { message: 'Username precisa ter pelo menos 5 caracteres' });
     maxLength(schema.username, 20, { message: 'Username precisa ter menos de 20 caracteres' });
@@ -31,11 +43,11 @@ export class Metadata {
     metadata(schema.username, USERNAME_HELP, (fieldContext) => {
       const value = fieldContext.value();
       const valueLength = value.length;
-      const minLength = fieldContext.state.minLength!()!
-      const maxLength = fieldContext.state.maxLength!()!
-      const touched = fieldContext.state.touched()
+      const minLength = fieldContext.state.minLength!()!;
+      const maxLength = fieldContext.state.maxLength!()!;
+      const touched = fieldContext.state.touched();
 
-      if(!touched) {
+      if (!touched) {
         if (valueLength === 0) {
           return `Digite um username com no mínimo ${minLength} e no máximo ${maxLength} caracteres`;
         }
@@ -51,5 +63,8 @@ export class Metadata {
 
       return '';
     });
+
+    metadata(schema.password, HELP_LIST, () => 'Mensagem 1');
+    metadata(schema.password, HELP_LIST, () => 'Mensagem 2');
   });
 }
